6ea769b35ffe709c896abdfb3bcccea331193040
Hari Govind S <hgs@disroot.org>
2019-02-03 17:13:34 +0530

    Updated SQL schema to match new Session Library of CI3

--- tables.sql
+++ tables.sql
@@ -81,13 +81,13 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 
 CREATE TABLE IF NOT EXISTS `ci_sessions` (
-  `session_id` varchar(40) NOT NULL DEFAULT '0',
+  `id` varchar(40) NOT NULL DEFAULT '0',
   `ip_address` varchar(45) NOT NULL DEFAULT '0',
   `user_agent` varchar(120) NOT NULL,
-  `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
-  `user_data` text NOT NULL,
-  PRIMARY KEY (`session_id`),
-  KEY `last_activity_idx` (`last_activity`)
+  `timestamp` int(10) unsigned NOT NULL DEFAULT 0,
+  `data` blob NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `last_activity_idx` (`timestamp`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
